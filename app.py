55882fd28e8ecb563a16c92df1427b064ecf1991
SahilPadhiyar18 <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-07-21 17:19:45 +0530

    s

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ def home_page():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
 
-    # Execute a command: this creates a new table
-    cur.execute('DROP TABLE IF EXISTS books;')
-    cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-                                     'title varchar (150) NOT NULL,'
-                                     'author varchar (50) NOT NULL,'
-                                     'pages_num integer NOT NULL,'
-                                     'review text,'
-                                     'date_added date DEFAULT CURRENT_TIMESTAMP);'
-                                     )
+#     # Execute a command: this creates a new table
+#     cur.execute('DROP TABLE IF EXISTS books;')
+#     cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+#                                      'title varchar (150) NOT NULL,'
+#                                      'author varchar (50) NOT NULL,'
+#                                      'pages_num integer NOT NULL,'
+#                                      'review text,'
+#                                      'date_added date DEFAULT CURRENT_TIMESTAMP);'
+#                                      )
 
-    # Insert data into the table
+#     # Insert data into the table
 
     cur.execute('INSERT INTO books (title, author, pages_num, review)'
                 'VALUES (%s, %s, %s, %s)',
