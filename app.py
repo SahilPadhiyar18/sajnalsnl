7bcf273c145c11e0b53084433fcdf26c0ae52e6a
SahilPadhiyar18 <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-07-21 17:03:33 +0530

    s

--- app.py
+++ app.py
@@ -9,9 +9,41 @@ app = Flask(__name__)
 def home_page():
     DATABASE_URL = 'postgres://qhkjicbidbltki:ff7899d0f2ef26bd2c754f6a7219d94b5c4d58de6073ad2d68d700eedb704473@ec2-44-197-128-108.compute-1.amazonaws.com:5432/d623bc8jc1utqt'
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
 
-#     DATABASE_URL = os.environ['DATABASE_URL']
-#     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # Execute a command: this creates a new table
+    cur.execute('DROP TABLE IF EXISTS books;')
+    cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
+                                     'title varchar (150) NOT NULL,'
+                                     'author varchar (50) NOT NULL,'
+                                     'pages_num integer NOT NULL,'
+                                     'review text,'
+                                     'date_added date DEFAULT CURRENT_TIMESTAMP);'
+                                     )
+
+    # Insert data into the table
+
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+                ('A Tale of Two Cities',
+                 'Charles Dickens',
+                 489,
+                 'A great classic!')
+                )
+
+
+    cur.execute('INSERT INTO books (title, author, pages_num, review)'
+                'VALUES (%s, %s, %s, %s)',
+                ('Anna Karenina',
+                 'Leo Tolstoy',
+                 864,
+                 'Another great classic!')
+                )
+    conn.commit()
+
+    cur.close()
+    conn.close()
+# Save and close the file.
     return '<h1>heo sahil padhiyar<h1>'
 
 @app.route('/math', methods=['POST'])  # This will be called from UI
